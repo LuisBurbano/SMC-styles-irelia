c6e90046993315e3d97839d4fd0e9582c5e01968
Luis Burbano <laburbano1@espe.edu.ec>
2022-05-22 21:03:22 -0500

    added classes prototype

---
+++ 06-Code/Stylist.java
@@ -0,0 +1,18 @@
+class Stylist{
+    private String name;
+    private String address;
+    private int number;
+    private float pendingPayment;
+    private String appointment;
+
+    private void slopesAppointment(){
+
+    }
+    private void removeAppointment(){
+
+    }
+    private void pay(){
+
+    }
+
+}
